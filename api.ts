edc397837a3ca1aded3acd6e33f04c731266107e
Stephen1393 <stephen.bending20@gmail.com>
2026-06-04 15:57:45 +0100

    Rewrite API setup with Node.js and TypeScript. Add node_modules to gitignore.

---
+++ api.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+
+app.listen(3000, function() {
+    console.log("listening on port 3000...")
+})
+
+app.get('/', function(req, res) {
+    
+    res.send("a homepage") 
+})
+
+
+
