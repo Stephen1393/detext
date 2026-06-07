97616e1ad9caf5a2d673216a2cee44c7edd42ca7
Stephen1393 <stephen.bending20@gmail.com>
2026-06-07 15:47:09 +0100

    connect to database using Client class and object

---
+++ database.ts
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv'
+import { Client } from 'pg'
+
+dotenv.config()
+
+const config = {
+    host:process.env.POSTGRES_HOST,
+    port:Number(process.env.POSTGRES_PORT),
+    user:process.env.POSTGRES_USER,
+    password:process.env.POSTGRES_PASSWORD,
+    database:process.env.POSTGRES_DB
+}
+
+
+const client = new Client(config) 
+
+console.log(config)
+
+try {
+ await client.connect()
+ console.log("connected")
+} catch (error) {
+    console.log(error)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1. Start Docker container
+//2. Find DB credentials
+//3. Read credentials from .env
+//4. Connect using pg Client
+//5. Create documents table
+//6. Insert document record
+//7. Query record back
