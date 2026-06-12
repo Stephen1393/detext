d9ebf43c1fb66f38670987d221e54d32728593c1
Stephen1393 <stephen.bending20@gmail.com>
2026-06-12 16:20:48 +0100

    add logic for getting document, checking id and document exist

--- api.ts
+++ api.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import multer from 'multer'
 import { storage } from './storage.js'
 import { extract_text } from './doc_process.js';
-import { insertDocument } from './database/documents.js';
+import { createDocument } from './database/documents.js';
 import { getDocuments, getDocument } from './database/documents.js'
 
 const upload = multer({ dest: 'uploads/' });
@@ -18,6 +18,7 @@ app.listen(3000, function() {
 app.get('/', function(req, res) {
     
     res.send("a homepage") 
+
 }) //end get
 
 
@@ -32,7 +33,7 @@ app.post("/documents", upload.single('document'), async function (req, res) {
     const savedPath = storage(uploadFile) //savedfilepath 
     const text = await extract_text(savedPath) //extracted text 
 
-    const database = await insertDocument(fileName,savedPath,text)
+    const database = await createDocument(fileName,savedPath,text)
     
     res.status(200).send(database)
 
@@ -45,8 +46,26 @@ app.get("/documents", async function(req, res) {
 
     console.log("GET documents route hit")
     
-    res.status(200).json({document: result})
-    
+    res.status(200).json({documents: result})
+
 }) //end get
 
+app.get("/document/:id", async function (req, res) {
+
+    const id = Number(req.params.id) 
+
+    if (Number.isNaN(id)) {
+
+        return res.status(400).send("no id found")
+    }
+
+    const result = await getDocument(id)
+
+    if (!result) {
+
+        return res.status(400).send("no document found")
+    }
+
+    res.status(200).json({document: result})
+})
 
